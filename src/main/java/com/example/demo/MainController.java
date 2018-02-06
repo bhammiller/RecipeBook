3cfec876d2d1df40f2f739b6e7dc2583b7a26115
Brandon Hammiller <bdayton1985@gmail.com>
2018-02-06 16:58:16 -0500

    Links Work Have created all of my HTML files.

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,4 +1,54 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class MainController {
+    @RequestMapping("/")
+    public String homePage(){
+        return "home";
+    }
+    @RequestMapping("tocrabcakes")
+    public String gotoCrabCakes(){
+        return "crabcakes";
+    }
+    @RequestMapping("toinjera")
+    public String gotoInjera(){
+        return "injerarecipe";
+    }
+    @RequestMapping("togulab")
+    public String gotoGulab(){
+        return "gulabjamunrecipe";
+    }
+    @RequestMapping("tojerk")
+    public String gotoJerk(){
+        return "jerkchicken";
+
+    }
+    @RequestMapping("tobara")
+    public String gotoBara(){
+        return "bara";
+
+    }
+    @RequestMapping("topumpkin")
+    public String gotoPumpkin(){
+        return "pumpkinricenoodles";
+
+    }
+    @RequestMapping("tocroque")
+    public String gotoCroque(){
+        return "croquemadame";
+    }
+    @RequestMapping("togallery")
+    public String gotoGallery(){
+        return "recipelist";
+
+    }
+    @RequestMapping("tohome")
+    public String gotoHome(){
+        return "home";
+
+    }
 }
